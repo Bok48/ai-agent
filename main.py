3c48e704acf125030ac78814e7381680019bca18
Bok48 <janolav.aspelund@gmail.com>
2025-10-06 21:39:41 +0200

    add: gemini conversation groundwork

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     # Prompt used for AI search is in the second argument
@@ -12,11 +13,16 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+     
+    user_prompt = sys.argv[1] # User input prompt
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
     
-    model = "gemini-2.0-flash-001" # Gemini model with free tier
-    contents = sys.argv[1] # User input prompt
-    
-    response = client.models.generate_content(model=model, contents=contents)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", # Gemini model with free tier 
+        contents=messages
+        )
     
     # Amount of tokens consumed
     prompt_tokens = response.usage_metadata.prompt_token_count

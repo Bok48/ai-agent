db6ea26c4f15ce023a7ffd373477bed1fcdd29a2
Bok48 <janolav.aspelund@gmail.com>
2025-10-06 17:06:27 +0200

    add: gemini ai response creation

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
 def main():
-    print("Hello from aiagent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    
+    model = "gemini-2.0-flash-001" # Gemini model with free tier
+    contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    
+    response = client.models.generate_content(model=model, contents=contents)
+    
+    # Amount of tokens consumed
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
+
+    print(response.text)
+
+    print(f"Prompt tokens: {prompt_tokens}")
+    print(f"Response tokens: {response_tokens}")
 
 
 if __name__ == "__main__":

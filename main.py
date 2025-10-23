bfd5bd91fb0a614691f4f92362946b22b174f7fe
Bok48 <janolav.aspelund@gmail.com>
2025-10-24 00:24:53 +0200

    add: agent can actually call funtions

--- main.py
+++ main.py
@@ -28,20 +28,25 @@ def main():
     ######## Readying all the content and parameters for the AI ########
     user_prompt = sys.argv[1] # User input prompt
     messages = [ # The message from the user to the AI
-        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+        types.Content(role="user", parts=[types.Part(text=f"User: {user_prompt}")]),
     ]
+
     # Parameter that defines the 'rules' of the conversation.
     system_prompt = """
     You are a helpful AI coding agent.
 
-    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+    When a user asks a question or makes a request, make a function call plan, and then run them directly.
+    You can perform the following operations:
 
     - List files and directories
-    - Read file contents
+    - Read file content
     - Write strings to files
     - Run Python files
 
     All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    Functions 'called' will be called on my system, and the results will be returned to you as a user prompt so that you can decide what to do next.
+    
+    When you return a text response, the entire dialogue will stop.
     """
     
     # Which functions are available for the AI to use from the local files
@@ -59,27 +64,53 @@ def main():
         system_instruction=system_prompt
     )
 
-    # Send instructions and retrieve the result from the AI
-    response = client.models.generate_content(
-        model="gemini-2.0-flash-001", # Gemini model with free tier 
-        contents=messages,
-        config=config,
-    )
-    
-    # Amount of tokens consumed
-    prompt_tokens = response.usage_metadata.prompt_token_count
-    response_tokens = response.usage_metadata.candidates_token_count
+    iteration = 1
+    while iteration <= 20:
+        #print(f"Iteration: {iteration}")
+        #print(f"Messages: {messages}")
+        try:
+            # Send instructions and retrieve the result from the AI
+            response = client.models.generate_content(
+                model="gemini-2.0-flash-001", # Gemini model with free tier 
+                contents=messages,
+                config=config,
+            )
 
-    print(response.text)
+            for candidate in response.candidates:
+                #print(f"Candidate: {candidate.content}")
+                messages.append(candidate.content)
+            
+            # Amount of tokens consumed
+            prompt_tokens = response.usage_metadata.prompt_token_count
+            response_tokens = response.usage_metadata.candidates_token_count
 
-    function_calls = response.function_calls
-    if function_calls is not None and len(function_calls) > 0:
-        for function_call in function_calls:
-            call_response = call_function(function_call)
-            if call_response.parts[0].function_response.response == None:
-                raise Exception("Fatal error: could not get response from the function call.")
-            if arg_verbose:
-                print(f"-> {call_response.parts[0].function_response.response}")
+            # Once response.text has content, the task from the prompt has been completed
+            if response.text is not None and response.function_calls is None:
+                print(response.text)
+                break
+        except Exception as e:
+            print(f"Error with response from Gemini: {e}")
+            break
+
+        try:
+            function_calls = response.function_calls
+            if function_calls is not None and len(function_calls) > 0:
+                for function_call in function_calls:
+                    call_response = call_function(function_call)
+                    if call_response.parts[0].function_response.response == None:
+                        raise Exception("Fatal error: could not get response from the function call.")
+                    for response in call_response.parts:
+                        if arg_verbose:
+                            print(f"-> {response.function_response.response}")
+                        messages += [ # Appending the function responses to be given back to AI
+                            types.Content(role="user", parts=[types.Part(text=f'Tool: Here is the result of {function_call.name}: {response.function_response.response}')]),
+                        ]
+            
+        except Exception as e:
+            print(f"Error calling function(s): {e}")
+            break
+        
+        iteration += 1
 
     if len(sys.argv) > 2:
 
